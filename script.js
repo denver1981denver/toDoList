ad8214bf799deee7b5fd53a6b7639f168df24855
denver1981denver <dennik1981@mail.com>
2021-06-28 15:45:15 +0300

    feat(script):functional changes

--- script.js
+++ script.js
@@ -4,50 +4,57 @@ const todoControl = document.querySelector('.todo-control'),
       headerInput = document.querySelector('.header-input'),
       todoList = document.querySelector('.todo-list'),
       todoCompleted = document.querySelector('.todo-completed'),
-      todoData = [];
-
+      todoData = JSON.parse(localStorage.getItem('todoList')) || [];
 
 const render = function() {
   todoList.textContent = '';
   todoCompleted.textContent = '';
   
-
-  todoData.forEach(function(item){
-  
-    const li = document.createElement('li');
-    li.classList.add('todo-item');
-     
-    li.innerHTML = '<span class="text-todo">' + item.value + '</span>' + '<div class="todo-buttons">' + '<button class="todo-remove"></button>' + '<button class="todo-complete"></button>' + '</div>';
+todoData.forEach(function(item, i) {
+  const li = document.createElement('li');
+  li.classList.add('todo-item'); 
+  li.innerHTML = '<span class="text-todo">' + item.value + '</span>' + '<div class="todo-buttons">' + '<button class="todo-remove"></button>' + '<button class="todo-complete"></button>' + '</div>';
        
-    if(item.completed){
+  if (item.completed){
       todoCompleted.append(li);
-    } else {
+  } else {
       todoList.append(li);
-    }
+  };
      
-   const btnTodoComplete = li.querySelector('.todo-complete');
+const btnTodoComplete = li.querySelector('.todo-complete');
       
   btnTodoComplete.addEventListener('click', function(){
     item.completed = !item.completed;
+    localStorage.setItem('todoList', JSON.stringify(todoData));
     render();
-      
     });
-   
+const btnTodoRemove = li.querySelector('.todo-remove');
+  
+  btnTodoRemove.addEventListener('click', function() {
+    todoData.splice(i, 1);
+    localStorage.setItem('todoList', JSON.stringify(todoData));
+    render();
   });
-   
+  }); 
 };
   
-todoControl.addEventListener('submit', function(event){
+todoControl.addEventListener('submit', function(event) {
   event.preventDefault();
-
+if (headerInput.value.trim() != '') {
   const newTodo = {
     value: headerInput.value,
     completed: false
   };
 
+  headerInput.value = "";
   todoData.push(newTodo);
+  localStorage.setItem('todoList', JSON.stringify(todoData));
+  render();
+  } else {
+  headerInput.value = '';
+  };
+  });
  
   render();
-});
 
-render();
+
